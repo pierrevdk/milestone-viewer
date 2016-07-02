4a7d65e6867f25ffdffa713d77d6bfceb4b7310f
Joseph Silvestre <joseph.silvestre38@gmail.com>
2016-07-02 16:55:43 +0200

    Add disabled state to timeline buttons

--- src/app/components/timeline.jsx
+++ src/app/components/timeline.jsx
@@ -5,26 +5,32 @@ export default function Timeline(props, context) {
     const location = context.history.getCurrentLocation();
 
     let previousLink;
+    let previousClass;
     if (props.moment > 0) {
         previousLink = context.history.createHref({
             pathname: location.pathname,
             query: { moment: props.moment - 1 },
         });
+    } else {
+        previousClass = 'disabled';
     }
 
     let nextLink;
+    let nextClass;
     if (props.moment + 1 < timeline.length) {
         nextLink = context.history.createHref({
             pathname: location.pathname,
             query: { moment: props.moment + 1 },
         });
+    } else {
+        nextClass = 'disabled';
     }
 
     return (
         <div className="timeline">
-            <a href={previousLink}>← Previous</a>
+            <a href={previousLink} className={previousClass}>← Previous</a>
             <div>{timeline[props.moment]} • {props.moment + 1}/6</div>
-            <a href={nextLink}>Next →</a>
+            <a href={nextLink} className={nextClass}>Next →</a>
         </div>
     );
 }
