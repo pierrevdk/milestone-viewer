67885c0885dcabca48aed0076bb47b5908dc5be0
Joseph Silvestre <joseph.silvestre38@gmail.com>
2016-07-04 10:18:55 +0200

    Add button goto first/last moment

--- src/app/components/timeline.jsx
+++ src/app/components/timeline.jsx
@@ -4,6 +4,8 @@ export default function Timeline(props, context) {
     const { timeline, moment } = props;
     const location = context.history.getCurrentLocation();
 
+    let firstLink;
+    let firstClass;
     let previousLink;
     let previousClass;
     if (moment > 0) {
@@ -11,10 +13,18 @@ export default function Timeline(props, context) {
             pathname: location.pathname,
             query: { moment: moment - 1 },
         });
+
+        firstLink = context.history.createHref({
+            pathname: location.pathname,
+            query: { moment: 0 },
+        });
     } else {
         previousClass = 'disabled';
+        firstClass = 'disabled';
     }
 
+    let lastLink;
+    let lastClass;
     let nextLink;
     let nextClass;
     if (moment + 1 < timeline.length) {
@@ -22,15 +32,22 @@ export default function Timeline(props, context) {
             pathname: location.pathname,
             query: { moment: moment + 1 },
         });
+
+        lastLink = context.history.createHref({
+            pathname: location.pathname,
+            query: { moment: timeline.length - 1 },
+        });
     } else {
         nextClass = 'disabled';
+        lastClass = 'disabled';
     }
 
     return (
         <div className="timeline">
+            <a href={firstLink} className={firstClass}>First</a>
             <a href={previousLink} className={previousClass}>← Previous</a>
-            <div>{timeline[moment]} • {moment + 1}/6</div>
             <a href={nextLink} className={nextClass}>Next →</a>
+            <a href={lastLink} className={lastClass}>Last</a>
         </div>
     );
 }
